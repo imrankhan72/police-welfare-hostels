febdc09a8990c0c7da654d6cc248b24a38ad9962
imrankhan72 <imrankhan.72@gmail.com>
2023-05-23 13:49:25 +0530

    Update messages.php

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -25,10 +25,11 @@ return [
     "all"=>"All",
     "contact_us"=>"Contact Us",
     "about_us_page" => "
-                    <p>Madhya Pradesh Police Department will provide Hostel facilities for the daughters of Police personnel who wish to stay in cities having better opportunities of higher education. The Police Welfare Branch, Madhya Pradesh has established three hostels in Bhopal, Indore and in Gwalior for the purpose. The department has named the initiative 'Police Welfare Girls Hostels'. Retired female cops to be hired for the post of warden in these hostels.</p>
-                     <p>Under the guidance of DGP Shri Sudhir Kumar Saxena Girls hostels have been established in Bhopal, Indore, and Gwalior districts. These hostels will ensure convenience to 'daughters' of the Police personnel (from Constables to Inspector rank) in pursuing their career. In Bhopal- 208, in Indore- 162 and Gwalior- 148 Girl student accommodations have been prepared, respectively. These will be managed by the district police force.</p>
-                    <p>A nominal fee will be charged from the inmates for their stay up to a maximum of four years in these hostels.
-Police Welfare branch wishes to encourage Police Personnel below the rank of Inspector to send their daughters for higher studies in big cities of Madhya Pradesh.</p>
+                    <p>Madhya Pradesh Police Department will provide Hostel facilities for the daughters of Police personnel who wish to stay in cities having better opportunities of higher education. The Police Welfare Branch, Madhya Pradesh has established three hostels in Bhopal, Indore and in Gwalior for the purpose. The department has named the initiative 'Police Welfare Girls Hostels'. Retired female police personnel to be hired for the post of warden in these hostels. </p>
+                    <p>Under the guidance of DGP Shri Sudhir Kumar Saxena, Girls hostels have been established in Bhopal, Indore, and Gwalior districts. These hostels will ensure convenience to 'daughters' of the Police personnel (from Constables to Inspector rank) in pursuing their career. In Bhopal- 208, in Indore- 162 and Gwalior- 148 Girl student accommodations have been prepared, respectively. These will be managed by the district police force. </p>
+                    <p>A nominal fee will be charged from the inmates for their stay up to a maximum of four years in these hostels. </p>
+                    <p>Police Welfare branch wishes to encourage Police Personnel below the rank of Inspector to send their daughters for higher studies in big cities of Madhya Pradesh. </p>
+
 ",
 "hostel_page" => "
 <h3>Explore our Hostels</h3>
@@ -46,60 +47,43 @@ Police Welfare branch wishes to encourage Police Personnel below the rank of Ins
 "facility_6"=> "R.O. water purifier",
 "facility_7"=> "Canteen for snacks and meals (As per requirement)",
 "facility_8"=> "Visitor's room for meeting family members",
-"facility_9"=> "Reliable tiffin services",
+"facility_9"=> "Link to reliable tiffin services",
 
 //eligibility page
 
 "eligibility_page"=>"<h3>Eligibility for Hostel Admission</h3>",
-"point_1"=>"Hostel admissions will be limited to the daughters of Police Personnel (from Constables to Inspector rank).",
+"point_1"=>"Hostel admissions will be limited to the 'daughters' of Police Personnel (from Constables to Inspector rank).",
 "point_2"=>"The applicant's age should be 18 years or above and below 26 years, as on January of the admission year.",
-"point_3"=>"For short term stays, minimum rent of 15 days will be charged",
+"point_3"=>"For short term stays, minimum rent of 15 days will be charged.",
 "point_4"=>"Admission will be valid for a period of one year, and applicants will need to reapply and renew their admission before July 1 for each subsequent year. ",
 "point_5"=>"An applicant can only be granted admission for a maximum of four years, either continuously or in separate periods not exceeding four years.",
-"point_6"=>"Any applicant exceeding the four-year limit will not be eligible for the hostel admission.",
-"point_7"=>"During the hostel admission process, an applicant must provide details of the educational institution/coaching center, along with information about the respective education/coaching program. Additionally, attach proof of admission to the educational institution/coaching center.",
+"point_6"=>"Any applicant exceeding the four-year limit will not be eligible for the hostel admission. ",
+"point_7"=>"During the hostel admission process, an applicant must provide details of the educational institution/coaching center, along with information about the respective education/coaching program. Additionally, attach proof of admission to the educational institution/coaching center. ",
 "point_8"=>"Allocation of rooms will be based on a first-come, first-served basis and any form of recommendation will render the candidate ineligible.",
 "point_9"=>"Admissions will commence from July 1, 2023 and will remain open for each academic year. ",
 "point_10"=>"Applicant students must fill and submit an admission form on the official website. ",
-"point_11"=>"Hostel admissions will be limited to the daughters of police officers (from constables to inspector rank). ",
-"point_12"=>"The applicant's age should be 18 years or above and below 26 years, as on January 1, 2023. ",
-"point_13"=>"Admission will be valid for a period of one year, and applicants will need to reapply and renew their admission before July 1 for each subsequent year.",
-"point_14"=>"An applicant can only be granted admission for a maximum of four years, either continuously or in separate periods not exceeding four years. ",
-"point_15"=>"Any applicant exceeding the four-year limit will not be eligible for the hostel admission. ",
-"point_16"=>"During the hostel admission process, an applicant must provide details of the educational institution/coaching center, along with information about the respective education/coaching program. Additionally, attach proof of admission to the educational institution/coaching center. ",
-"point_17"=>"Allocation of rooms will be based on a first-come, first-served basis, and any form of recommendation will be render the candidate. ",
-"point_18"=>"In the case of two equally eligible candidates, the Girls residing in bhopal city, will not be eligible for admission in Bhopal Hostel, likewise Girls residing in Indore city will not be eligible for admission in Indore Hostel; as also Girls residing in Gwalior city will not be eligible for admission in Gwalior Hostel. ",
 
 //rules-regulations
 "point_1"=>"Students must reside in the designated dormitories and cannot shift the rooms at their own will. ",
-"point_2"=>"Student Committees along with the Committee, Sanitation Committee, Discipline Committee, Procurement Committee, and Management Committee will be established to work for the welfare of the students.",
-"point_3"=>"Since all residents will be above 18 years of age, they should not expect to be treated as dependents by their parents or the administration and should take responsibility for their personal lives. ",
-"point_4"=>"Residents must adhere to the prescribed dress code. Maintaining decorum in attire and behavior is essential. ",
-"point_5"=>"Smoking, alcohol, and consumption of intoxicating substances in and around the hostel premises are strictly prohibited. In any such cases, immediate eviction from the hostel will be enforced. ",
-"point_6"=>"In case of any quarrels or violation of peace, those found guilty through enquiry process will face penalties and eviction. ",
-"point_7"=>"The administration and staff will not be responsible for the security of personal belongings. ",
-"point_8"=>"Making noise or engaging in activities that may cause physical or psychological harm or pose a threat is strictly prohibited. ",
-"point_9"=>"Students are required to keep the premises and rooms clean. Defacing walls, equipment, furniture, etc., is strictly prohibited.",
-"point_10"=>"A fine of 200 rupees will be imposed for a dirty room, and a collective penalty of 2000 rupees will be imposed on the wing if the entire gallery/bathroom/toilet area is found dirty. A collective penalty of 3000 rupees will be imposed for an entire floor being dirty. ",
-"point_11"=>"No student is allowed to use electrical stoves, heaters, irons, or similar appliances in their rooms, except in designated areas for this purpose. ",
-"point_12"=>"Meals will only be served in specified dining halls and during designated times. ",
-"point_13"=>"All lights in the rooms must be turned off by 11 PM, except for study lamps, which are allowed.",
-"point_14"=>"Visitors/parents can meet students in the visitor's room between 4:30 PM and 6:30 PM. Visitors are not allowed to leave the visiting area. ",
-"point_15"=>"External guests/students are not allowed inside the hostel premises. ",
-"point_16"=>"Any complaints related to electricity, plumbing, etc. should be recorded in the complaint register. ",
-"point_17"=>"Management is not responsible for arranging meetings between students residing inside or outside the hostel premises. ",
-"point_18"=>"Students are responsible for their transportation to college, coaching institutes, or other places. ",
-"point_19"=>"They are required to get their own newspapers. ",
-"point_20"=>"In case of illness, students must arrange for their own treatment. In extreme circumstances, the management will make an effort to provide the nearest and best available facilities. ",
-"point_21"=>"If parents want to take their daughter home during vacation days, they need to obtain written permission from the warden. ",
-"point_22"=>"If any candidate wants to vacate the hostel, they must give a one-month notice in writing. ",
-"point_23"=>"Before giving any information or interview in the press, radio, television, social media, or any other media, or or before delivering any speech related to the hostel or any information associated with the hostel, it is required to get prior written permission from the warden. ",
-"point_24"=>"If any candidate stays outside the hostel for even a day without providing written notice to the hostel administration, that candidate will be expelled from the hostel. ",
-"point_25"=>"The candidates shall not be involved in any political or communal activities that are detrimental to law and order, nor shall they engage in any propaganda related to these activities. ",
-"point_26"=>"Candidate shall bring their own bedding, necessary clothes (warm and regular), and blankets, etc. The materials provided by the hostel cannot be taken out of the rooms. If any material goes missing or gets damaged, the respective student(s) will have to compensate for it at the end of the session. ",
-"point_27"=>"It is mandatory to adhere to all the rules of the hostel. ",
-"point_28"=>"Residents are responsible for the security of their personal belongings.",
-"point_29"=>"Any vacant duration during occupancy will be charged. ",
+"point_2"=>"Residents are responsible for the security of their personal belongings.",
+"point_3"=>"Residents must adhere to the prescribed dress code. Maintaining decorum in attire and behavior is essential. ",
+"point_4"=>"Smoking, alcohol, and consumption of intoxicating substances in and around the hostel premises are strictly prohibited. In any such cases, immediate eviction from the hostel will be enforced. ",
+"point_5"=>"In case of any quarrels or violation of peace, those found guilty through enquiry process will face penalties and eviction. ",
+"point_6"=>"The administration and staff will not be responsible for the security of personal belongings. ",
+"point_7"=>"Making noise or engaging in activities that may cause physical or psychological harm or pose a threat is strictly prohibited. ",
+"point_8"=>"Students are required to keep the premises and rooms clean. Defacing walls, equipment, furniture, etc., is strictly prohibited. ",
+"point_9"=>"No student is allowed to use electrical stoves, heaters, irons, or similar appliances in their rooms, except in designated areas for this purpose. ",
+"point_10"=>"All lights in the rooms must be turned off by 11 PM, except for study lamps, which are allowed. ",
+"point_11"=>"Visitors/parents can meet students in the visitor's room between 4:30 PM and 6:30 PM. Visitors are not allowed to leave the visiting area.  ",
+"point_12"=>"External guests/students are not allowed inside the hostel premises. ",
+"point_13"=>"Any complaints related to electricity, plumbing, etc. should be recorded in the complaint register. ",
+"point_14"=>"Management is not responsible for arranging meetings between students residing inside or outside the hostel premises. ",
+"point_15"=>"Students are responsible for their transportation to college, coaching institutes, or other places. ",
+"point_16"=>"They are required to get their own newspapers.  ",
+"point_17"=>"Any vacant duration during occupancy will be charged. ",
+"point_18"=>"If any candidate stays outside the hostel for even a day without providing written notice to the hostel administration, that candidate will be expelled from the hostel. ",
+"point_19"=>"Candidate shall bring their own bedding, necessary clothes (warm and regular), and blankets, etc. The materials provided by the hostel cannot be taken out of the rooms. If any material goes missing or gets damaged, the respective student(s) will have to compensate for it at the end of the session. ",
+"point_20"=>"Candidate shall bring their own bedding, necessary clothes (warm and regular), and blankets, etc. The materials provided by the hostel cannot be taken out of the rooms. If any material goes missing or gets damaged, the respective student(s) will have to compensate for it at the end of the session. ",
 
 
 //Photo Gallery
@@ -115,7 +99,7 @@ Police Welfare branch wishes to encourage Police Personnel below the rank of Ins
 
 
 "faq_2"=>"Which ranks of police officers and employees can have their daughters admitted to the hostel? ",
-"faq_list2"=>"Daughters of police officers and employees ranging from constables to inspector levels can be admitted to the hostel. ",
+"faq_list2"=>"Daughters of police officers and employees ranging from Constables to Inspector levels can be admitted to the hostel. ",
 
 
 "faq_3"=>"How many years can one receive education while staying in the hostel? ",
@@ -173,5 +157,3 @@ Police Welfare branch wishes to encourage Police Personnel below the rank of Ins
 "send_messgae"=>"Send Message",
 
 ];
-
-
