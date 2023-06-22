628110e5030990bf72ebb90e26e44d0284260fee
imrankhan72 <imrankhan.72@gmail.com>
2023-06-22 14:17:07 +0530

    Update hostels.blade.php

--- resources/views/website/hostels.blade.php
+++ resources/views/website/hostels.blade.php
@@ -47,7 +47,7 @@
                         <div class="icon-box">
                             <div class="icon"><img src="assets/img/gwalior.jpg" class="img-fluid" alt=""></div>
                             <h4><a href="">{{ __('messages.gwalior') }}</a></h4>
-                            <p>148 {{ __('messages.hostel_seats') }}</p>
+                            <p>148 {!! __('messages.hostel_seats') !!}</p>
                             <div class="btn-wrap pricing pt-3">
                                 <a href="apply.html" class="btn-buy">{{ __('messages.apply_now') }}</a>
                             </div>
