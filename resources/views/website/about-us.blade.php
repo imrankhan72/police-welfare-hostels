3cd14b3beb3120dfa0db5a4e7e9b12a4d540c60d
shubhambrij <137378459+shubhambrij@users.noreply.github.com>
2023-06-22 17:24:43 +0530

    Update about-us.blade.php

--- resources/views/website/about-us.blade.php
+++ resources/views/website/about-us.blade.php
@@ -6,10 +6,10 @@
     <section class="breadcrumbs">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
-                <h2>{{ __('messages.contact_us') }}</h2>
+                <h2>{{ __('messages.about_us') }}</h2>
                 <ol>
                     <li><a href="#">{{ __('messages.home') }}</a></li>
-                    <li>{{ __('messages.contact_us') }}</li>
+                    <li>{{ __('messages.about_us') }}</li>
                 </ol>
             </div>
         </div>
